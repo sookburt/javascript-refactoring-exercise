51550029d6005adc035898cc0d8fc3d79b07ad1b
Sue Burt <sookburt@gmail.com>
2022-02-28 12:53:59 +0000

    Using an arrow function to create return array

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -6,24 +6,17 @@ function processTransactions(transactions) {
     // changed from global to local variable
     const txr = [];
 
-    let txCount = {}
+    let txCount = {};
 
-    // TODO: required?  if so, use directly.
-    const numberOfTransactions = transactions.length;
-
-    // TODO: change to declarative
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transactions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
+    // changed - to declarative 
+    transactions.forEach(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
 
     // TODO: ? re-assign to new
     txCount = sortByAmountThenName(txCount);
-    
+
     // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
+    // changed - to use an arrow function.
+    Object.keys(txCount).forEach((key, index) => txr[index] = `${key} ${txCount[key]}`);
 
     return txr;
 }
