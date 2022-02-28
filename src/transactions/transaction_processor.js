b9d25c8e927de08bd7d2f62a7f510bf4bbb63437
Sue Burt <sookburt@gmail.com>
2022-02-28 13:52:19 +0000

    Renamed variables

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -3,35 +3,35 @@ function processTransactions(transactions) {
      // change to one line and move to top also moved test directly into conditional statment.
     if (transactions === undefined) throw new Error("Undefined collection of transactions");
 
-    // changed from global to local variable
-    const txr = [];
+    // changed from global to local variable and renamed.
+    const processedTransactions = [];
 
-    let txCount = {};
+    const transactionsCount = {};
 
     // changed - to declarative 
-    transactions.forEach(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
+    transactions.forEach(item => transactionsCount[item] ? transactionsCount[item] += 1 : transactionsCount[item] = 1);
 
     // TODO: ? re-assign to new
-    txCount = sortByAmountThenName(txCount);
+    const sortedTransactionsCount = sortByAmountThenName(transactionsCount);
 
     // Place them back in array for returning
     // changed - to use an arrow function.
-    Object.keys(txCount).forEach((key, index) => txr[index] = `${key} ${txCount[key]}`);
+    Object.keys(sortedTransactionsCount).forEach((key, index) => processedTransactions[index] = `${key} ${sortedTransactionsCount[key]}`);
 
-    return txr;
+    return processedTransactions;
 }
 
-function sortByAmountThenName(txCount) {
+function sortByAmountThenName(transactionsCount) {
 
     // changed  - create function expression to use as a callback function for sort method 
-    const sortingFunction = (itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo);
+    const sortingFunction = (itemOne, itemTwo) => transactionsCount[itemTwo] - transactionsCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo);
 
     // changed - using new sorting callback
-    let sortedKeys = Object.keys(txCount).sort(sortingFunction);
+    let sortedKeys = Object.keys(transactionsCount).sort(sortingFunction);
 
     let sortedResults = {};
     for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
+        sortedResults[objectKey] = transactionsCount[objectKey];
     }
 
     return sortedResults;
