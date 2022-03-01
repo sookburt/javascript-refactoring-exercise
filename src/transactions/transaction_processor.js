fd05ac897eeca64c14dd80f34b1c9a76fdb27085
Sue Burt <sookburt@gmail.com>
2022-03-01 09:18:55 +0000

    Removed comments

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,19 +1,12 @@
-// changed transActions to transactions - one word therefore camel case inappropriate
 function processTransactions(transactions) {
-     // change to one line and move to top also moved test directly into conditional statement.
     if (transactions === undefined) throw new Error("Undefined collection of transactions");
 
-    // changed from global to local variable and renamed then declare on same line.
     const [processedTransactions, transactionsCount] = [[], {}];
 
-    // changed - to declarative 
     transactions.forEach(item => transactionsCount[item] ? transactionsCount[item] += 1 : transactionsCount[item] = 1);
 
-    // changed - re-assign to new
     const sortedTransactionsCount = sortByAmountThenName(transactionsCount);
 
-    // Place them back in array for returning
-    // changed - to use an arrow function.
     Object.keys(sortedTransactionsCount).forEach((key, index) => processedTransactions[index] = `${key} ${sortedTransactionsCount[key]}`);
 
     return processedTransactions;
@@ -23,10 +16,8 @@ function sortByAmountThenName(transactionsCount) {
 
     let sortedResults = {};
 
-    // changed - using arrow function
     let sortedKeys = Object.keys(transactionsCount).sort((itemOne, itemTwo) => transactionsCount[itemTwo] - transactionsCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo));
 
-    // changed - using forEach to iterate
     sortedKeys.forEach(objectKey =>  sortedResults[objectKey] = transactionsCount[objectKey] );
 
     return sortedResults;
